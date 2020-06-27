dfd2f33b916a9f362a0430c0b9b7f3da0233f0be
Shadow Walker <66953516+shadowwalker005@users.noreply.github.com>
2020-06-27 23:52:45 +0500

    Add files via upload

---
+++ style.sh
@@ -0,0 +1,131 @@
+#!/system/bin/sh
+
+#text colors
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+pink=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+
+bt=$(tput bold 2)
+#reset color
+
+reset=$(tput sgr0)
+
+
+#function
+
+config() {
+    touch bash.bashrc
+    
+
+    echo "red=$(tput setaf 1)" >> bash.bashrc
+    echo "green=$(tput setaf 2)" >> bash.bashrc
+    echo "yellow=$(tput setaf 3)" >> bash.bashrc
+
+    echo "pink=$(tput setaf 5)" >> bash.bashrc
+    echo "cyan=$(tput setaf 6)" >> bash.bashrc
+    echo "white=$(tput setaf 7)" >> bash.bashrc
+
+    echo "bt=$(tput bold 2)" >> bash.bashrc
+    echo "clear" >> bash.bashrc
+    echo "figlet -f big $bname | lolcat" >> bash.bashrc
+    echo "PS1='${bt}${cyan}┌$uname┐${yellow}╱\W╱${green}╼╾⌬╼╾Ξ '" >> bash.bashrc
+    mv bash.bashrc $PREFIX/etc/bash.bashrc
+}
+clear
+echo  "${cyan}${bt}"
+echo " ~ Please Wait Checking For Requirements"
+echo
+sleep 3
+ru=$PREFIX/bin/ruby
+if [ -f "$ru" ]; then
+    echo " ${green}${bt}~ Ruby Found"
+    sleep 2
+else 
+    echo " ~ Installing Ruby"
+    sleep 2
+    echo
+    pkg install ruby
+fi
+echo
+lol=$PREFIX/bin/lolcat
+if [ -f "$lol" ]; then
+     echo " ${green}${bt}~ LoLCat Found"
+     sleep 2
+else 
+    echo " ~ Installing LoLCat"
+    sleep 2
+    echo
+    gem install lolcat
+fi
+echo
+figl=$PREFIX/bin/figlet
+if [[ -f "$figl" ]]; then
+    echo " ${green}${bt}~ Figlet Found"
+    sleep 2
+else
+    echo " ~ Installing Figlet"
+    sleep 2
+    echo
+    gem install figlet
+fi
+clear
+echo
+
+            
+            
+            
+#banner for shadow
+
+
+echo "${red}░██████╗██╗░░██╗░█████╗░██████╗░░█████╗░░██╗░░░░░░░██╗"
+echo "${red}██╔════╝██║░░██║██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║"
+echo "${red}╚█████╗░███████║███████║██║░░██║██║░░██║░╚██╗████╗██╔╝"
+echo "${red}░╚═══██╗██╔══██║██╔══██║██║░░██║██║░░██║░░████╔═████║░"
+echo "${red}██████╔╝██║░░██║██║░░██║██████╔╝╚█████╔╝░░╚██╔╝░╚██╔╝░"
+echo "${red}╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚════╝░░░░╚═╝░░░╚═╝░░"
+
+            
+            
+            
+            
+echo "${cyan}${bt}"
+intro() {
+echo " ~ Style By Ihtisham Ali - Shadow Walker "
+echo
+echo " ~ YouTube Channel: https://tiny.cc/ShadowYT"
+echo
+}
+echo
+intro
+echo
+echo
+echo "${green}${bt}"
+read -p " ~ Enter Banner Name: ${yellow}${bt}" bname
+echo "${white}${bt} ~ Username: ${pink}${bt}$bname"
+echo "${green}${bt}"
+read -p " ~ Enter Username: ${yellow}${bt}" uname
+echo " ~ ${white}${bt}Banner Name:${pink}${bt}$uname"
+echo																				
+echo
+echo
+read -p "${green}${bt} ~ Press Enter To Activate..."
+clear
+echo
+echo "${green}${bt} ~ Please Wait Setting Up Configuration"
+echo
+config
+sleep 3
+echo  "${green}${bt} ~ Applying Stlye..."
+echo
+sleep 3
+echo "${green}${bt} ~ Style Applied Successfully!"
+echo
+echo
+echo "${red}${bt} ~ Subscribe My YouTube Channel"
+echo "${red}${bt} ~ Channel Link: https://tiny.cc/ShadowYT"
+echo
