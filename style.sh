0f86abbfbc7696d191b9fbb7cf1b8f62bd586110
Shadow Walker <66953516+shadowwalker005@users.noreply.github.com>
2020-07-18 13:12:21 +0500

    Update style.sh

--- style.sh
+++ style.sh
@@ -17,20 +17,42 @@ reset=$(tput sgr0)
 
 
 cd
+clear
+echo " ${blue}${bt}[${white}*${blue}] ${green}Installing Dependencies"
+echo
+echo " ${blue}${bt}[${white}*${blue}] ${green}Installing Curl"
+echo
 pkg install curl -y
+echo " ${blue}${bt}[${white}*${blue}] ${green}Installing Zsh"
+echo
 pkg install zsh -y
+echo " ${blue}${bt}[${white}*${blue}] ${green}Installing Figlet"
+echo
 pkg install figlet -y
+echo " ${blue}${bt}[${white}*${blue}] ${green}Installing Color-Scheme"
+echo
 pkg install ncurses-utils -y
+echo " ${blue}${bt}[${white}*${blue}] ${green}Installing Oh-My-Zsh"
+echo
 curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+echo " ${blue}${bt}[${white}*${blue}] ${green}Configuring Oh-My-Zsh"
+echo
 yes "n" | bash install.sh -y
-
+echo " ${blue}${bt}[${white}*${blue}] ${green}Setting Up Shadow Theme"
+echo
 sed -i "s/robbyrussell/shadowwalker/g" .zshrc
+sleep 3
+
 read -p " Enter Banner Name: " banner_name
 read -p " Enter Username: " user_name
+echo
+read -p " Hit Enter To Apply Settings..."
+
 sed -i "s/ihtisham/$user_name/g" $HOME/style/shadowwalker.zsh-theme
 sed -i "s/shadow/$banner_name/g" $HOME/style/shadowwalker.zsh-theme
 sed -i "s/robbyrussell/shadowwalker/g" .zshrc
 cp -r $HOME/style/shadowwalker.zsh-theme $HOME/.oh-my-zsh/themes
-yes "zsh" | chsh
+yes "zsh" | chsh 
 rm -rf $HOME/style
-echo "Please Restart Termux"
+echo
+echo " Please Restart Termux"
